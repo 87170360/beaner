3fef77d6bc15054e3339ecb6f4e0608dd9a605df
BookHeart <87170360@qq.com>
2017-02-17 15:48:56 +0800

    sort beaner

--- main.cc
+++ main.cc
@@ -18,24 +18,40 @@ void swapDna(int p1[], int p2[], int ret[], int size)
     ret[point] = rand() % BEHAVIOR;
 }
 
-void selectChampion(int beaner[], int size, int ret[])
+void selectChampion(Beaner all[], int weight[], int size, int ret[])
+{
+
+}
+
+void sortBeaner(Beaner all[])
 {
 
 }
 
 int main()
 {
+/* test swapDna
     int x[] = {1,2,3,4,5,6};
     int y[] = {11,22,33,44,55,66};
     int b[] = {0,0,0,0,0,0};
     swapDna(x, y, b, 6);
-    //int a[] = {5,6,7};
-
-    //std::copy(a, a + 3, b);
-
     for(int i = 0; i < sizeof(b) / sizeof(int); ++i)
     {
         std::cout << b[i] << " " ;
     }
     std::cout << std::endl;
+*/
+
+
+    //all beaner
+    Beaner m_all[BEANER_NUM];
+    //weight
+    int m_weight[BEANER_NUM];
+    for(int i = 0; i < BEANER_NUM; ++i)
+    {
+        Beaner bean = Beaner();
+        bean.m_score = i;
+        m_all[i] = bean; 
+        m_weight[i] = BEANER_NUM - i;
+    }
 }
